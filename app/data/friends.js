a4425696910021c5c3b293638070e7e64e858c11
crhodes <connorrhodes1@yahoo.com>
2018-01-12 03:01:24 -0800

    mostly complete, didn't commit until the home page worked.

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'John Snow',
+		photo: 'https://i.pinimg.com/originals/12/46/01/124601331d2ab34fc5702733308c066f.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jaime Lannister',
+		photo: 'https://fsmedia.imgix.net/57/f1/e4/9b/765d/49a3/9085/a85552d88d38/jamie-lannister-1024jpg.jpeg?rect=0%2C0%2C1024%2C512&fm=png&w=1200',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Daenerys Targaryen',
+		photo: 'https://vignette.wikia.nocookie.net/gameofthrones/images/8/88/Daenerys_Targaryen_S7_Promo_Image.PNG/revision/latest?cb=20170420191154',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Tyrion Lannister',
+		photo: 'http://www.konbini.com/wp-content/blogs.dir/11/files/2017/08/tyrion-feat.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Cersei Lannister',
+		photo: 'http://media.immediate.co.uk/volatile/sites/3/2017/08/promo324259790.png',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Sansa Stark',
+		photo: 'https://i.pinimg.com/736x/5d/0d/cc/5d0dcc875f607e61a2de6f76e52566a6--dark-queen-ice-queen.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Arya Stark',
+		photo: 'http://www1.pictures.zimbio.com/mp/QPZb2IE60Agl.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Margaery Tyrell',
+		photo: 'https://pbs.twimg.com/profile_images/713228404980523008/LjZqdtoP.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Robb Stark',
+		photo: 'https://vignette1.wikia.nocookie.net/gameofthrones/images/e/e9/Robb-3x03.jpg/revision/latest?cb=20130413125346',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Eddard Stark',
+		photo: 'https://memegenerator.net/img/images/600x600/15205788/got-winter-is-comingggg.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
